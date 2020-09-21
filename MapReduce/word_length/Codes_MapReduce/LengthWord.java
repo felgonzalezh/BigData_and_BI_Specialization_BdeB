7acf532bb9c04e624e4fe48935ddb89ebeb9d154
felgonzalezh <felgonzalezh@gmail.com>
2020-09-21 15:46:14 -0400

    Adding project to find the word length

---
+++ MapReduce/word_length/Codes_MapReduce/LengthWord.java
@@ -0,0 +1,34 @@
+package com.degenio.hadoop;         
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text; 
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.io.IntWritable; 
+import org.apache.hadoop.io.IntWritable; 
+import org.apache.hadoop.mapred.FileOutputFormat; 
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
+
+public class LengthWord {
+      	public static void main(String[] args) throws Exception {  
+	JobConf conf = new JobConf(LengthWord.class); 
+	conf.setJobName("LengthWord");
+
+	conf.setOutputKeyClass(Text.class); 
+	conf.setOutputValueClass(IntWritable.class); 
+	
+	conf.setMapperClass(MonMap.class);
+//	conf.setCombinerClass(MonReduce.class); Hadoop does not garantee that Combiner is applied (for all the cases, and even it might not be applied at all). Best case scenario is go directly from mapper to reducer 
+	conf.setReducerClass(MonReduce.class);
+	
+	conf.setInputFormat(TextInputFormat.class);
+	conf.setOutputFormat(TextOutputFormat.class);
+
+	FileInputFormat.setInputPaths(conf, new Path(args[0])); 
+  	FileOutputFormat.setOutputPath(conf, new Path(args[1])); 
+    	
+	JobClient.runJob(conf);                                                                                                                        
+  }                                                                                                                                                  
+                                                                                                                                                     
+}
