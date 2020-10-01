1506a356bd6e6f01b546cda85761d3404865ef17
felgonzalezh <felgonzalezh@gmail.com>
2020-10-01 16:11:51 -0400

    Implemented loop over a main data file to extract csv files data

--- Python/ETL_Project_Clients/mod_main.py
+++ Python/ETL_Project_Clients/mod_main.py
@@ -1,27 +1,28 @@
+# main to execute the ETL process
+
+from mod_classes import Registre_Client
 from mod_extraction import Extraction
-from mod_load import writing_data
-
-
-class Transformation(object):
-    pass
-
 
 def main():
-    # Read JSON file with whole data files:
-    # for each file:
-    
-    # Extraction:
-    Extraction()
-    # for each file, mod_extraction will return a list of objects Client
-    
-    # Transformation
-    Transformation()
 
-    # Load
-    writing_data()
+    file_in = "data_config.json"
+    etl = Extraction(file_in)
+
+    # Loop over formats of files
+    for file_format, data_set in etl.etl_data["data_sources"].items():
+        # Loop over files. We pass the key to get the file
+        for file in data_set:
+            # Extraction
+            listing = Registre_Client()
+            etl = Extraction(file_in)
+            if file_format == "csv":
+                etl.reading_csv(file,listing)
+                print("=" * 50)
+            else:
+                print("Unknown format")
+            listing.print_client()
 
 
 
 if __name__ == '__main__':
     main()
-    
