78467e5d9102728c935f603f755500eb708d9c1e
felgonzalezh <felgonzalezh@gmail.com>
2020-10-02 08:20:41 -0400

    Gender transformation implemented

--- Python/ETL_Project_Clients/mod_transformation.py
+++ Python/ETL_Project_Clients/mod_transformation.py
@@ -1,6 +1,13 @@
-# This module contains a function with each transformaition needed.
-def genre():
-    pass
+# Transformation class
+class Transformation:
+    def __init__(self,data_list):
+        self.data_list = data_list
 
-def ville():
-    pass
+    def genre(self):
+        for elem in self.data_list.registre:
+            if elem.genre == "Female":
+                elem.genre = "F"
+            elif elem.genre == "Male":
+                elem.genre = "M"
+            else:
+                elem.genre = ""
